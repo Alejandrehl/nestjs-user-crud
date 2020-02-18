c381495bad520174036327a1aa265a9883b83a5f
Alejandro Exequiel Hernández Lara <Alejandrehl@icloud.com>
2020-02-18 16:54:53 -0300

    Mockup basico de controladores

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,25 @@
-import { Controller, Post } from '@nestjs/common';
-import { create } from 'domain';
+import { Controller, Post, Body, Get, Put, Delete } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user-dto';
 
 @Controller('users')
 export class UsersController {
   @Post()
-  create(@Body createUserDto: CreateUserDto) {}
+  create(@Body() CreateUserDto: CreateUserDto) {
+    return 'Usuario Creado';
+  }
+
+  @Get()
+  getAll() {
+    return 'Lista de Usuarios';
+  }
+
+  @Put(':id')
+  update(@Body() updateUserDto: CreateUserDto) {
+    return 'Usuario Actualizado';
+  }
+
+  @Delete(':id')
+  delete() {
+    return 'Usuario Eliminado';
+  }
 }
