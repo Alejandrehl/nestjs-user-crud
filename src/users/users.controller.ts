13be89803751dd965fe86e426798e101c37d773c
Alejandro Exequiel Hernández Lara <Alejandrehl@icloud.com>
2020-02-18 16:13:02 -0300

    save

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,8 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { create } from 'domain';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Post()
+  create(@Body createUserDto: CreateUserDto) {}
+}
