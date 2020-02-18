a6df05421d3b7bb5455e5fa66401ff703ea5c25c
Alejandro Exequiel Hernández Lara <Alejandrehl@icloud.com>
2020-02-18 17:21:57 -0300

    Conectando correctamente el proyecto a la base de datos MySQL, al ejecutar el proyecto se generan automaticamente los modelos en la BD

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 8889,
+      username: 'alejandro',
+      password: '',
+      database: 'nestjs-user-crud',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController, UsersController],
   providers: [AppService],
 })
